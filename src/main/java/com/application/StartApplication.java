db6bb509e284fa556ece04d79b22c611e80d0969
Dineth Shakya <150885267+DinethShakya23@users.noreply.github.com>
2026-02-28 08:44:59 +0530

    change UI

--- src/main/java/com/application/StartApplication.java
+++ src/main/java/com/application/StartApplication.java
@@ -12,8 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD. Test03, Test04");
+        model.addAttribute("title", "CI/CD Pipeline Successfully Deployed");
         return "index";
     }
 
